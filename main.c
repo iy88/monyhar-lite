8549573bb6f984b2b55000a469e14b9ebf58824d
WenisOK <65259149+WenisOK@users.noreply.github.com>
2022-05-07 18:41:25 +0800

    新增浏览器中文提示，方便国内用户方便使用

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
-int surf_internet() {
-  return 0;
+int surf_internet(){
+	printf("欢迎使用梦弘浏览器\n无法访问互联网资源，因为改浏览器没有上网功能。\n");
+	return 0;
 }
-
 int clean_cache(){
-  return 0;
+	printf("正在清理浏览器缓存\n无法清理缓存，因为改浏览器不产生任何缓存数据。");
+	return 0;
 }
-
-int main() {
-  return surf_internet();
+int main(){
+	surf_internet();
+	clean_cache();
+	return 0;
 }
